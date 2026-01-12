afb51f426d789fb4576e31c70add1b4c1cd7f298
Steve Young <60384755+steveyoung74@users.noreply.github.com>
2026-01-12 20:06:06 +0000

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'evoke-v1';
+const ASSETS = [
+  '/evoke.html',
+  '/Evoke_Large_Dk_2_burst.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
